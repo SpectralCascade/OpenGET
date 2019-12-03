2efc1b0e420d7416c3293afa34e47ce44841c896
SpectralCascade <timlanesoftware@gmail.com>
2019-12-03 19:16:32 +0000

    Added a couple more logging methods

--- Unity/Log.cs
+++ Unity/Log.cs
@@ -9,36 +9,40 @@ namespace OpenGET
     public static class Log
     {
 
+        public static void Debug(string message, params object[] args) {
+            UnityEngine.Debug.Log("<color=purple>Info: " + string.Format(message, args) + "</color>");
+        }
+
         /// <summary>
         /// Logs information.
         /// </summary>
         public static void Info(string message, params object[] args) {
-            Debug.Log("<color=magenta>Info: " + string.Format(message, args) + "</color>");
+            UnityEngine.Debug.Log("<color=magenta>Info: " + string.Format(message, args) + "</color>");
         }
 
         /// <summary>
         /// Logs warnings.
         /// </summary>
         public static void Warning(string message, params object[] args) {
-            Debug.Log("<color=yellow>Warning: " + string.Format(message, args) + "</color>");
+            UnityEngine.Debug.Log("<color=yellow>Warning: " + string.Format(message, args) + "</color>");
         }
 
         /// <summary>
         /// Logs errors.
         /// </summary>
         public static void Error(string message, params object[] args) {
-            Debug.Log("<color=red>Error: " + string.Format(message, args) + "</color>");
+            UnityEngine.Debug.Log("<color=red>Error: " + string.Format(message, args) + "</color>");
         }
 
         public static void Verbose(string message, params object[] args) {
-            Debug.Log("<color=cyan>Info: " + string.Format(message, args) + "</color>");
+            UnityEngine.Debug.Log("<color=cyan>Info: " + string.Format(message, args) + "</color>");
         }
 
         /// <summary>
         /// Logs exceptions.
         /// </summary>
         public static void Exception(Exception e) {
-            Debug.LogError(e.ToString());
+            UnityEngine.Debug.LogError(e.ToString());
         }
 
     }
