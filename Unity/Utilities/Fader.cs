12404946e55a1fd80357e7ae99a5efb9af8de1f6
Trinity Lane <trinity@usepod.net>
2024-01-09 10:21:18 +0000

    Faders now support use of unscaled time, not just scaled time

--- Unity/Utilities/Fader.cs
+++ Unity/Utilities/Fader.cs
@@ -31,19 +31,26 @@ namespace OpenGET
         private bool isVisible { get { return implementation.GetValue() > 0; } }
 
         /// <summary>
+        /// Whether unscaled time should be used for the fade or not. Defaults to true.
+        /// </summary>
+        private bool useUnscaledTime = true;
+
+        /// <summary>
         /// Use CanvasGroup implementation.
         /// </summary>
         /// <param name="canvasGroup"></param>
-        public Fader(CanvasGroup canvasGroup) {
+        public Fader(CanvasGroup canvasGroup, bool useUnscaledTime = true) {
             implementation = new CanvasGroupFader(canvasGroup);
+            this.useUnscaledTime = useUnscaledTime;
         }
 
         /// <summary>
         /// Use Renderer implementation (e.g. MeshRenderer, SpriteRenderer).
         /// </summary>
         /// <param name="renderer"></param>
-        public Fader(Renderer renderer) {
+        public Fader(Renderer renderer, bool useUnscaledTime = true) {
             implementation = new RendererFader(renderer);
+            this.useUnscaledTime = useUnscaledTime;
         }
 
         /// <summary>
@@ -111,13 +118,13 @@ namespace OpenGET
         /// <param name="end"></param>
         /// <param name="time"></param>
         public IEnumerator Fade(float start, float end, float time = 1.0f) {
-            float startTime = Time.time;
+            float startTime = useUnscaledTime ? Time.unscaledTime : Time.time;
             float elapsedTime = 0;
             float lerpValue = 0;
             time = Mathf.Max(float.Epsilon, time);
 
             while (true) {
-                elapsedTime = Time.time - startTime;
+                elapsedTime = (useUnscaledTime ? Time.unscaledTime : Time.time) - startTime;
                 lerpValue = elapsedTime / time;
 
                 implementation.SetValue(Mathf.Lerp(start, end, lerpValue));
