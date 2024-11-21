de6d746ac1ac6ffc273f4b7dd94bc7e07b7c1d47
Trinity Lane <trinity@usepod.net>
2024-11-21 17:25:16 +0000

    WIP JSON serialisation (piggy-backing on Newtonsoft JSON library)

--- Unity/Serialisation/Serialiser.cs
+++ Unity/Serialisation/Serialiser.cs
@@ -1,31 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 namespace OpenGET
 {
 
     /// <summary>
+    /// Implement this for own game if you want easy reflection-based serialisation with versioning.
+    /// </summary>
+    public abstract class SerialiseAttribute : Attribute
+    {
+        /// <summary>
+        /// Serialisation version. It is recommended that you implement this as an enum in your derived class,
+        /// which you update with a new version for every release build.
+        /// </summary>
+        public int version;
+
+        /// <summary>
+        /// Former name this variable was serialised as. Used to handle the renaming of a variable so old saves still work.
+        /// </summary>
+        public string formerName;
+
+        /// <summary>
+        /// Whether this variable has been removed from serialisation for this version.
+        /// </summary>
+        public bool removed;
+
+        public SerialiseAttribute(int version, string formerName = null, bool removed = false)
+        {
+            this.version = version;
+            this.formerName = formerName;
+            this.removed = removed;
+        }
+    }
+
+    /// <summary>
     /// Base class for implementing a custom serialisation system.
     /// </summary>
-    public abstract class Serialiser<T, VersionType> where T : Serialiser<T, VersionType>
+    public abstract class Serialiser<Derived, Version, VarAttribute> where VarAttribute : SerialiseAttribute where Derived : Serialiser<Derived, Version, VarAttribute>
     {
+
+        /// <summary>
+        /// Whether to serialise or deserialise data.
+        /// </summary>
+        public enum Mode
+        {
+            Serialise = 0,
+            Deserialise = 1
+        }
 
         /// <summary>
         /// Implement on classes you wish use custom serialisation logic for.
         /// </summary>
         public interface ISerialise
         {
-            /// <summary>
-            /// Deserialise data.
-            /// </summary>
-            public void Deserialise(T serialiser);
-
-            /// <summary>
-            /// Serialise data.
-            /// </summary>
-            public void Serialise(T serialiser);
-
+            public void Serialise(Derived s);
         }
 
         /// <summary>
@@ -36,27 +66,70 @@ namespace OpenGET
         /// <summary>
         /// Current serialisation version.
         /// </summary>
-        public virtual VersionType version { get; protected set; }
+        public virtual Version version { get; protected set; }
+
+        /// <summary>
+        /// Is this serialiser in serialising or deserialising mode?
+        /// </summary>
+        public Mode mode { get; protected set; }
+
+        /// <summary>
+        /// Whether to serialise or deserialise data.
+        /// </summary>
+        public bool isWriting => mode == Mode.Serialise;
 
         /// <summary>
         /// Write to the file at path.
         /// </summary>
-        public abstract void Save();
+        public abstract Result Save(ISerialise game);
 
         /// <summary>
         /// Read from the file at path.
         /// </summary>
-        public abstract void Load();
+        public abstract Result Load(ISerialise game);
 
         /// <summary>
-        /// Serialise the given data into a valid format for saving.
+        /// Serialise/deserialise a value (depending on serialiser mode).
         /// </summary>
-        public abstract void Serialise<DataType>(ref DataType data);
+        public void Serialise<DataType>(string id, ref DataType data)
+        {
+            if (isWriting)
+            {
+                Write(id, data);
+            }
+            else
+            {
+                Read(id, ref data);
+            }
+        }
 
         /// <summary>
         /// Deserialise an object from the serialised format.
         /// </summary>
-        public abstract void Deserialise<DataType>(ref DataType data);
+        public abstract bool Read<DataType>(string id, ref DataType data);
+
+        /// <summary>
+        /// Serialise the given data into a valid format for saving.
+        /// </summary>
+        public abstract void Write<DataType>(string id, DataType data);
+
+        /// <summary>
+        /// Serialise an object using it's custom serialisation handler.
+        /// </summary>
+        public virtual void Serialise(ISerialise data)
+        {
+            mode = Mode.Serialise;
+            data.Serialise((Derived)this);
+        }
+
+        /// <summary>
+        /// Deserialise an object using it's custom serialisation handler.
+        /// </summary>
+        public virtual void Deserialise(ISerialise data)
+        {
+            mode = Mode.Deserialise;
+            data.Serialise((Derived)this);
+        }
 
     }
 
--- Unity/Serialisation/SerialiserJSON.cs
+++ Unity/Serialisation/SerialiserJSON.cs
@@ -2,6 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Reflection;
+using System;
+using System.Linq;
 
 namespace OpenGET
 {
@@ -9,30 +14,239 @@ namespace OpenGET
     /// <summary>
     /// JSON serialisation implementation.
     /// </summary>
-    public abstract class SerialiserJSON<T, VersionType> : Serialiser<SerialiserJSON<T, VersionType>, VersionType> where T : SerialiserJSON<T, VersionType>
+    public abstract class SerialiserJSON<Derived, VersionType, VarAttribute> : Serialiser<Derived, VersionType, VarAttribute> where VarAttribute : SerialiseAttribute where Derived : SerialiserJSON<Derived, VersionType, VarAttribute>
     {
+        protected JObject json = new JObject();
+
+        protected delegate void HandleToken(JToken token);
+
         /// <summary>
-        /// Serialised JSON data.
+        /// Ensures that properties are not serialised, a la Unity.
         /// </summary>
-        protected Dictionary<string, string> data = new Dictionary<string, string>();
+        protected class UnityContractResolver : Newtonsoft.Json.Serialization.DefaultContractResolver
+        {
+            protected override IList<Newtonsoft.Json.Serialization.JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+            {
+                List<FieldInfo> members = new List<FieldInfo>();
+                Type unityObjType = typeof(UnityEngine.Object);
+
+                for (Type evalType = type; evalType != null; evalType = evalType.BaseType)
+                {
+                    FieldInfo[] fields = evalType.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                    for (int i = 0, counti = fields.Length; i < counti; i++)
+                    {
+                        // Don't serialise Unity Objects or explicitly non-serialised members
+                        FieldInfo field = fields[i];
+                        if (field.IsNotSerialized
+                            || field.GetCustomAttribute<NonSerializedAttribute>() != null
+                            || unityObjType.IsAssignableFrom(field.FieldType)
+                            || (
+                                !field.IsPublic
+                                && field.GetCustomAttribute<SerializeField>() == null
+                                && field.GetCustomAttribute<VarAttribute>() == null
+                            )
+                        )
+                        {
+                            continue;
+                        }
+                        members.Add(field);
+                    }
+                }
+
+                members.Sort((a, b) => a.MetadataToken - b.MetadataToken);
+
+                List<Newtonsoft.Json.Serialization.JsonProperty> properties = new List<Newtonsoft.Json.Serialization.JsonProperty>(members.Count);
+                for (int i = 0, counti = members.Count; i < counti; i++)
+                {
+                    int index = properties.FindIndex((Newtonsoft.Json.Serialization.JsonProperty current) => current.UnderlyingName == members[i].Name);
+                    if (index >= 0)
+                    {
+                        continue;
+                    }
+                    Newtonsoft.Json.Serialization.JsonProperty property = CreateProperty(members[i], memberSerialization);
+                    property.Writable = true;
+                    property.Readable = true;
+                    properties.Add(property);
+                }
+                return properties;
+            }
+        }
+
+        /// <summary>
+        /// Serialiser using Unity-style contract resolver.
+        /// </summary>
+        protected JsonSerializer serial = new JsonSerializer();
+
+        public SerialiserJSON()
+        {
+            serial.ContractResolver = new UnityContractResolver();
+        }
+
+        public override Result Save(ISerialise game)
+        {
+            Serialise(game);
+
+            // Attempt to convert the dictionaries to JSON
+            string raw = json.ToString();
+            try
+            {
+                File.WriteAllText(path, raw, System.Text.Encoding.UTF8);
+                Log.Info("Saved game to {0}", path);
+            }
+            catch (System.Exception e)
+            {
+                Log.Exception(e);
+                return new Result(Localise.Text("Failed to save game: {0}", e.Message), e);
+            }
+            return new Result();
+        }
+
+        public override Result Load(ISerialise game)
+        {
+            Deserialise(game);
+
+            try
+            {
+                string raw = File.ReadAllText(path, System.Text.Encoding.UTF8);
+                JsonConvert.PopulateObject(raw, (Derived)this);
+            }
+            catch (System.Exception e)
+            {
+                Log.Exception(e);
+                return new Result(Localise.Text("Failed to load game: {0}", e.Message), e);
+            }
+            return new Result();
+        }
+
+        public delegate void HandleMember(FieldInfo field);
+
+        /// <summary>
+        /// Walk over members and selectively serialise them via reflection.
+        /// </summary>
+        protected void WalkSerialiseMembers(object data, HandleMember handler)
+        {
+            // Use reflection to selectively choose what we serialise
+            Type type = data.GetType();
+            FieldInfo[] fields = type.GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+            for (int i = 0, counti = fields.Length; i < counti; i++)
+            {
+                FieldInfo field = fields[i];
+                VarAttribute[] info = field.GetCustomAttributes<VarAttribute>().ToArray();
+                if (!(info.Length > 0 && info.Last().removed) &&
+                    (
+                        (field.IsPublic && field.GetCustomAttribute<NonSerializedAttribute>() == null)
+                        || field.GetCustomAttribute<SerializeField>() != null
+                        || field.GetCustomAttribute<VarAttribute>() != null
+                    )
+                ) {
+                    handler(field);
+                }
+            }
+        }
+
+        public override bool Read<DataType>(string id, ref DataType data)
+        {
+            return false;
+        }
+
+        public override void Write<DataType>(string id, DataType data)
+        {
+            // Make sure custom serialisation runs
+            if (data == null)
+            {
+                return;
+            }
+            else if (data is ISerialise custom)
+            {
+                JObject prev = json;
+                json = new JObject();
+                prev.Add(id, json);
+                custom.Serialise((Derived)this);
+                json = prev;
+            }
+            else // TODO: Provide class attribute that ensures we ADDITIONALLY do this step for ISerialise implementations
+            {
+                JToken token = JToken.FromObject(data, serial);
+                if (token.Type == JTokenType.Object)
+                {
+                    // Nested object
+                    JObject prev = json;
+                    json = new JObject();
+                    prev.Add(id, json);
+
+                    // Use reflection to selectively choose what we serialise
+                    WalkSerialiseMembers(data, (field) => {
+                        DataType scoped = data;
+                        Write(field.Name, field.GetValue(scoped));
+                    });
+
+                    if (data is IDictionary dict)
+                    {
+                        IEnumerable<string> keys = dict.Keys.OfType<string>();
+                        if (keys.Count() > 0)
+                        {
+                            foreach (string key in keys)
+                            {
+                                Write(key, dict[key]);
+                            }
+                        }
+                    }
+
+                    json = prev;
+                }
+                else if (token.Type == JTokenType.Array && data is IEnumerable)
+                {
+                    void HandleArray(JArray jArray, IEnumerable array)
+                    {
+                        foreach (object child in array)
+                        {
+                            JToken arrayToken = JToken.FromObject(child, serial);
+                            if (child == null)
+                            {
+                            }
+                            else if (arrayToken.Type == JTokenType.Array && child is IEnumerable)
+                            {
+                                JArray nested = new JArray();
+                                jArray.Add(nested);
+                                HandleArray(nested, child as IEnumerable);
+                            }
+                            else if (child is ISerialise custom)
+                            {
+                                JObject prev = json;
+                                json = new JObject();
+                                jArray.Add(json);
+                                custom.Serialise((Derived)this);
+                                json = prev;
+                            }
+                            else if (arrayToken.Type == JTokenType.Object)
+                            {
+                                JObject prev = json;
+                                json = new JObject();
+                                jArray.Add(json);
+                                WalkSerialiseMembers(child, (field) => {
+                                    object scoped = child;
+                                    Write(field.Name, field.GetValue(scoped));
+                                });
+                                json = prev;
+                            }
+                            else
+                            {
+                                jArray.Add(arrayToken);
+                            }
+                        }
+                    }
+
+                    JArray jArray = new JArray();
+                    json.Add(id, jArray);
+                    HandleArray(jArray, data as IEnumerable);
+                }
+                else
+                {
+                    json.Add(id, token);
+                }
+            }
+        }
         
-        public override void Save()
-        {
-            
-        }
-
-        public override void Load()
-        {
-        }
-
-        public override void Serialise<DataType>(ref DataType data)
-        {
-        }
-
-        public override void Deserialise<DataType>(ref DataType data)
-        {
-        }
-
     }
 
 }
